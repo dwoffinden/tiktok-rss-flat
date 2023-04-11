501cd935d4e09cd921d6705d3001e111c5befa89
Daniel Woffinden <daw@google.com>
2023-04-11 14:20:34 +0100

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -122,7 +122,7 @@ def run(csvuser):
         fg.author( {'name':'Conor ONeill','email':'conor@conoroneill.com'} )
         fg.link( href='http://tiktok.com', rel='alternate' )
         fg.logo(ghPagesURL + 'tiktok-rss.png')
-        fg.subtitle('OK Boomer, all the latest TikToks from ' + csvuser)
+        fg.subtitle('All the latest TikToks from ' + csvuser)
         fg.link( href=ghPagesURL + 'rss/' + csvuser + '.xml', rel='self' )
         fg.language('en')
 
